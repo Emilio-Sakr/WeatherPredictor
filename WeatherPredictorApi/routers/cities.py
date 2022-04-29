7f7f3eebb85565a8a4c2e0fc3b999520755676c5
BlightLantern <emiliosakr00@gmail.com>
2022-04-30 01:29:20 +0300

    Get weather history #4

--- WeatherPredictorApi/routers/cities.py
+++ WeatherPredictorApi/routers/cities.py
@@ -36,5 +36,22 @@ async def getWeather(city: str):
         raise HTTPException(status_code=404, detail="City not available")
     else:
         response = requests.get(f'https://api.open-meteo.com/v1/forecast?latitude={allCitiesJson[city][0]}&longitude={allCitiesJson[city][1]}&current_weather=true').json()
-        return [response['current_weather']['temperature']]
+        return {'temperature':response['current_weather']['temperature'],'unit':'celsius'}
+
+@Cities.get('/weather/{day}/{city}')
+async def getWeather(day: int, city: str):
+    if day < -2 or day > 6:
+        raise HTTPException(status_code=404, detail='specified past day should be between -2 and 6')
+    allCitiesJson = findCitiesJson()
+    if city not in allCitiesJson:
+        raise HTTPException(status_code=404, detail="City not available")
+    else:
+        day+=2
+        response = requests.get(f'https://api.open-meteo.com/v1/forecast?latitude={allCitiesJson[city][0]}&longitude={allCitiesJson[city][1]}&daily=temperature_2m_max,temperature_2m_min&timezone=EET&past_days=2').json()
+        daily = response['daily']
+        return {
+        'max_temperature':daily['temperature_2m_max'][day], 'min_temperature': daily['temperature_2m_min'][day],
+        'average_temperature':(int(daily['temperature_2m_max'][day])-int(daily['temperature_2m_min'][day]))/2,
+        }
+
     
